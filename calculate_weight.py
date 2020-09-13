6573b59bb8c8cb839eec3a1d7550cdbff77a6e66
Yohei_Hamada <2ykgh6@gmail.com>
2020-09-13 09:41:43 +0900

    [add]get distibuted representations

--- calculate_weight.py
+++ calculate_weight.py
@@ -1,7 +1,12 @@
 import clean_data as clean
 import dist_repre as dist
 
-
-# load dataset
+# Load dataset
 titles, categories = clean.cleanse_dataset('Dataset.txt')
 
+titles = dist.remove_stopwords(titles)
+
+assert len(titles) == len(categories), 'Not match dataset'
+
+# Get distributed representations 
+model = dist.get_dist_representations(titles)
--- dist_repre.py
+++ dist_repre.py
@@ -1,16 +1,37 @@
-import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from gensim.models import word2vec
 
-# stopwordの削除
 def remove_stopwords(titles):
+    '''remove stopwords from titles
+
+    # Argument
+    	titles = titles of dataset
+        
+    # Return
+    	removed_stopwords_titles = titles of removing stopwords
     '''
-    '''
+    # get stopwords
     stop_words = set(stopwords.words('english'))
+
     removed_stopwords_titles = []
+
+    # remove stopwords
     for title in titles:
         word_tokens = word_tokenize(title)
-        removed_stopwords_titles = [w for w in word_tokens if not w in stop_words]
+        removed_stopwords_title = [w for w in word_tokens if not w in stop_words]
+        removed_stopwords_titles.append(removed_stopwords_title)
 
     return removed_stopwords_titles
 
+def get_dist_representations(titles):
+    '''get distibuted representations
+
+    # Argument
+    	titles = titles of dataset
+
+    # Return
+    	model = vectors of words
+    '''
+    model = word2vec.Word2Vec(titles, size=200, min_count=3, window=5)
+    return model
