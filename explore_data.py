9b263a12fdcaf2e8c0f4234abc80156b3c02bafa
Yohei_Hamada <2ykgh6@gmail.com>
2020-09-29 15:52:44 +0900

    [add]print each bins

--- explore_data.py
+++ explore_data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# function --------------------------------------------------------------
+
 def read_dataset(filename):
     '''read dataset file
 
@@ -25,18 +27,36 @@ def read_dataset(filename):
 
     return titles, labels
 
-def plot_title_length_distribution(titles):
+BINS = 40
+
+def plot_title_length_distribution(titles, bins):
     '''plots the title length distribution
 
-    # Argument
+    # Arguments
         titles: list, titles of dataset
+        bins: int, number of bins
     '''
-    plt.hist([len(s.split()) for s in titles], bins=40)
+    plt.hist([len(s.split()) for s in titles], bins=bins)
     plt.xlabel('length of a title')
     plt.ylabel('Number of titles')
     plt.title('Title length distribution')
     plt.show()
 
+def print_each_bin(titles, bins):
+    '''print the values of the histogram
+
+    # Arguments
+        titles: list, titles of dataset
+        bins: int, number of bins
+    '''
+    hists, _ = np.histogram([len(s.split()) for s in titles], bins=bins)
+    for index, hist in enumerate(hists):
+        print('length of a title {0}: {1}'.format(index + 1, hist))
+
+# function --------------------------------------------------------------
 
 titles, labels = read_dataset('Dataset.txt')
-plot_title_length_distribution(titles)
+
+#plot_title_length_distribution(titles, BINS)
+
+#print_each_bin(titles, BINS)
